ce0eeada743e969c9a1c474e0a1570ef220f07be
Paul Colfer <68826277+paulcolfer@users.noreply.github.com>
2022-03-30 21:08:07 +0100

    Update dockerbackup.sh

--- dockerbackup.sh
+++ dockerbackup.sh
@@ -3,7 +3,7 @@ BACKUPS="/mnt/backup"
 # docker volumes are stored in this folder
 VOLUMES="/var/lib/docker/volumes"
 
-#Docker Volume to Backup
+#Docker volume to backup (Postgres in this example)
 docker stop Postgres
 zip -r --symlinks $BACKUPS/postgres-"`date +"%Y-%m-%d"`".zip $VOLUMES/postgres_data || true
 docker start Postgres
