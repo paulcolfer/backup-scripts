d526a5dc570282b40f765deeb9a8265572d26ebb
Paul Colfer <68826277+paulcolfer@users.noreply.github.com>
2022-04-09 00:29:13 +0100

    Update dockerbackup.sh
    Switch to using iso date format instead of manually building the filename

--- dockerbackup.sh
+++ dockerbackup.sh
@@ -2,10 +2,12 @@
 BACKUPS="/mnt/backup"
 # docker volumes are stored in this folder
 VOLUMES="/var/lib/docker/volumes"
+# get the iso date format
+NOW=$(date --iso)
 
 #Docker volume to backup (Postgres in this example)
 docker stop Postgres
-zip -r --symlinks $BACKUPS/postgres-"`date +"%Y-%m-%d"`".zip $VOLUMES/postgres_data || true
+zip -r --symlinks $BACKUPS/postgres-$NOW".zip $VOLUMES/postgres_data || true
 docker start Postgres
 
 #repeat the above block for each container
